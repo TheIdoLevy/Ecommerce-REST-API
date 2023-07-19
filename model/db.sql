d14eb41aae4c8705eec0c0c2a94721bfb2868011
TheIdoLevy <118935178+TheIdoLevy@users.noreply.github.com>
2023-07-19 21:55:28 +0300

    Update db.sql

--- model/db.sql
+++ model/db.sql
@@ -12,9 +12,9 @@ CREATE TABLE products(
   id serial PRIMARY KEY,
   name varchar(20) UNIQUE NOT NULL,
   category varchar(20),
-  price money,
-  price_per_kilo money,
-  discount varchar(20),
+  price integer,
+  price_per_kilo integer,
+  discount integer,
   amount_in_stock varchar(20),
   img varchar(60)
 );
@@ -24,13 +24,12 @@ CREATE TABLE carts(
   id serial,
   customer_id integer REFERENCES customers(id),
   product_id integer REFERENCES products(id) ON UPDATE CASCADE ON DELETE CASCADE,
-  quatity varchar(10)
 );
 
 
 CREATE TABLE checked_out_carts(
   customer_id integer REFERENCES customers(id),
-  total_price money,
+  total_price integer,
   date date,
   time time,
   payment_method varchar(20)
